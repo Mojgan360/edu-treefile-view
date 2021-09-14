5591b63b9598988778fa1a9f4b76da7cb8a9d059
Mojgan360 <mojgan360@gmail.com>
2021-09-14 12:52:49 +0200

    Added style

--- src/components/File/TreeFile.style.js
+++ src/components/File/TreeFile.style.js
@@ -1,3 +1,10 @@
 import styled from 'styled-components/macro'
 
-export const StyledFile = styled.div``
+export const StyledFile = styled.div`
+  color: #23395b;
+  flex-wrap: nowrap;
+  display: flex;
+  align-items: center;
+  font-weight: normal;
+  padding-left: ${(p) => p.theme.indent}px;
+`
--- src/components/Folder/TreeFolder.style.js
+++ src/components/Folder/TreeFolder.style.js
@@ -1,6 +1,10 @@
 import styled from 'styled-components/macro'
 
 export const StyledFolder = styled.section`
-
+  color: #23395b;
+  font-weight: bold;
+  padding-left: ${(p) => p.theme.indent}px;
+  .tree__file {
+    padding-left: ${(p) => p.theme.indent}px;
   }
 `
