2553fe7d1352caa2fef14018c705688a2d2751f3
Mojgan360 <mojgan360@gmail.com>
2021-09-14 13:13:25 +0200

    clean-up

--- src/Tree/File/TreeFile.js
+++ src/Tree/File/TreeFile.js
@@ -1,46 +1,46 @@
-import React, { useRef, useState } from "react";
-import { AiOutlineFile, AiOutlineDelete, AiOutlineEdit } from "react-icons/ai";
+import React, { useRef, useState } from 'react'
+import { AiOutlineFile, AiOutlineDelete, AiOutlineEdit } from 'react-icons/ai'
 
-import { StyledFile } from "Tree/File/TreeFile.style";
-import { useTreeContext } from "Tree/state/TreeContext";
-import { ActionsWrapper, StyledName } from "Tree/Tree.style.js";
-import { PlaceholderInput } from "Tree/TreePlaceholderInput";
+import { StyledFile } from 'Tree/File/TreeFile.style'
+import { useTreeContext } from 'Tree/state/TreeContext'
+import { ActionsWrapper, StyledName } from 'Tree/Tree.style.js'
+import { PlaceholderInput } from 'Tree/TreePlaceholderInput'
 
-import { FILE } from "Tree/state/constants";
-import FILE_ICONS from "Tree/FileIcons";
+import { FILE } from 'Tree/state/constants'
+import FILE_ICONS from 'Tree/FileIcons'
 
 const File = ({ name, id, node }) => {
-  const { dispatch, isImparative, onNodeClick } = useTreeContext();
-  const [isEditing, setEditing] = useState(false);
-  const ext = useRef("");
+  const { dispatch, isImparative, onNodeClick } = useTreeContext()
+  const [isEditing, setEditing] = useState(false)
+  const ext = useRef('')
 
-  let splitted = name?.split(".");
-  ext.current = splitted[splitted.length - 1];
+  let splitted = name?.split('.')
+  ext.current = splitted[splitted.length - 1]
 
-  const toggleEditing = () => setEditing(!isEditing);
+  const toggleEditing = () => setEditing(!isEditing)
   const commitEditing = (name) => {
-    dispatch({ type: FILE.EDIT, payload: { id, name } });
-    setEditing(false);
-  };
+    dispatch({ type: FILE.EDIT, payload: { id, name } })
+    setEditing(false)
+  }
   const commitDelete = () => {
-    dispatch({ type: FILE.DELETE, payload: { id } });
-  };
+    dispatch({ type: FILE.DELETE, payload: { id } })
+  }
   const handleNodeClick = React.useCallback(
     (e) => {
-      e.stopPropagation();
-      onNodeClick({ node });
+      e.stopPropagation()
+      onNodeClick({ node })
     },
     [node]
-  );
+  )
   const handleCancel = () => {
-    setEditing(false);
-  };
+    setEditing(false)
+  }
 
   return (
-    <StyledFile onClick={handleNodeClick} className="tree__file">
+    <StyledFile onClick={handleNodeClick} className='tree__file'>
       {isEditing ? (
         <PlaceholderInput
-          type="file"
+          type='file'
           style={{ paddingLeft: 0 }}
           defaultValue={name}
           onSubmit={commitEditing}
@@ -57,7 +57,7 @@ const File = ({ name, id, node }) => {
             &nbsp;&nbsp;{name}
           </StyledName>
           {isImparative && (
-            <div className="actions">
+            <div className='actions'>
               <AiOutlineEdit onClick={toggleEditing} />
               <AiOutlineDelete onClick={commitDelete} />
             </div>
@@ -65,7 +65,7 @@ const File = ({ name, id, node }) => {
         </ActionsWrapper>
       )}
     </StyledFile>
-  );
-};
+  )
+}
 
-export { File };
+export { File }
--- src/Tree/Tree.style.js
+++ src/Tree/Tree.style.js
@@ -1,4 +1,4 @@
-import styled from "styled-components/macro";
+import styled from 'styled-components/macro'
 
 export const StyledTree = styled.div`
   line-height: 1.75;
@@ -7,7 +7,7 @@ export const StyledTree = styled.div`
   .tree__input {
     width: auto;
   }
-`;
+`
 
 export const ActionsWrapper = styled.div`
   width: 100%;
@@ -43,33 +43,33 @@ export const ActionsWrapper = styled.div`
     pointer-events: all;
     transition: 0.2s;
   }
-`;
+`
 
 export const StyledName = styled.div`
   background-color: white;
   display: flex;
   align-items: center;
   cursor: pointer;
-`;
+`
 
 export const Collapse = styled.div`
   height: max-content;
-  max-height: ${p => (p.isOpen ? "800px" : "0px")};
+  max-height: ${(p) => (p.isOpen ? '800px' : '0px')};
   overflow: hidden;
   transition: 0.3s ease-in-out;
-`;
+`
 
 export const VerticalLine = styled.section`
   position: relative;
   :before {
-    content: "";
+    content: '';
     display: block;
     position: absolute;
-    top: -2px; /* just to hide 1px peek */
+    top: -2px;
     left: 1px;
     width: 0;
     height: 100%;
     border: 1px solid #dbdbdd;
     z-index: -1;
   }
-`;
+`
